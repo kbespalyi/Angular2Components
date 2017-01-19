a2866c85546a33b00b6fd8c84e3f6cb5ed781c7e
Khusein Bespalyi <khusein.bespalyi@otolane.com>
2017-01-19 18:17:21 -0500

    added models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'app works!';
+  clock = Observable.interval(1000);
+
+  constructor() {
+    console.log('App running...');
+
+    this.clock.subscribe(value => {
+      const tick = value;
+      
+    });
+  }
+
 }
